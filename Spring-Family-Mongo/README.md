455a481344451e5c63c0b0142ff2ddb630162634
Sierou-Java <javayue@yeah.net>
2017-10-19 00:04:45 +0800

    commit mongodb by sierou 🍪

--- Spring-Family-Mongo/README.md
+++ Spring-Family-Mongo/README.md
@@ -53,3 +53,45 @@ mongo
 
 **然后就会启动一个shell，到这里我们就可以开始操作我们数据库了**
 
+
+## 如何创建用户管理员
+
+
+**用户管理员是第一个要创建的用户。在没有创建任何用户之前，你可以随意创建用户；但数据库中一旦有了用户，那么未登录的客户端就没有权限做任何操作了，除非使用db.auth(username, password)方法登录。**
+
+用户管理员的角色名叫 userAdminAnyDatabase，这个角色只能在 admin 数据库中创建。下面是一个例子：
+
+```bash
+use admin
+switched to db admin
+> db.createUser({user:"sierou",pwd:"sierou",roles:["userAdminAnyDatabase"]})
+Successfully added user: { "user" : "sierou", "roles" : [ "userAdminAnyDatabase" ] }
+```
+
+这个例子创建了一个名为 root 的用户管理员。创建完了这个用户之后，我们应该马上以该用户的身份登录：
+
+```bash
+db.auth("sierou","sierou")
+1
+db.auth() 方法返回 1 表示登录成功。接下来我们为指定的数据库创建访问所需的账号。
+```
+
+---
+
+## 如何创建数据库用户
+
+
+首先保证你已经以用户管理员的身份登录 admin 数据库。然后用 use 命令切换到目标数据库，同样用 db.createUser() 命令来创建用户，其中角色名为 “readWrite”。
+
+普通的数据库用户角色有两种，read 和 readWrite。顾名思义，前者只能读取数据不能修改，后者可以读取和修改。
+下面是一个例子：
+
+```bash
+use test
+switched to db test
+> db.createUser({user:"test",pwd:"sierou",roles:["readWrite"]})
+Successfully added user: { "user" : "test", "testoles" : [ "readWrite" ] }
+> db.auth("test","sierou")
+1
+
+```
--- Spring-Family-Mongo/src/test/java/org/family/mongo/SpringFamilyMongdoApplicationTests.java
+++ Spring-Family-Mongo/src/test/java/org/family/mongo/SpringFamilyMongdoApplicationTests.java
@@ -1,7 +1,11 @@
 package org.family.mongo;
 
+import org.family.mongo.bean.UserEntity;
+import org.family.mongo.dao.UserDao;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringFamilyMongdoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
+    private UserEntity user = new UserEntity();
+
+    @Autowired
+    private UserDao userDao;
+
+    @Before
+    public void init() {
+        user.setId(2l);
+        user.setUserName("sierou");
+        user.setPassWord("sierou");
+    }
+
+    @Test
+    public void contextLoads() {
+
+        userDao.saveUser(user);
+
+        UserEntity user = userDao.findUserByUserName("sierou");
+        System.out.println("user is " + user);
+    }
 
 }
