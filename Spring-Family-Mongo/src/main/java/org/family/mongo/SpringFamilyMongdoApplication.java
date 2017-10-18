d1c27d683cd263871a42eb0ec2a2784ab09733ce
Sierou-Java <javayue@yeah.net>
2017-10-18 23:14:32 +0800

    commit mongodb by sierou 🍪

---
+++ Spring-Family-Mongo/src/main/java/org/family/mongo/SpringFamilyMongdoApplication.java
@@ -0,0 +1,12 @@
+package org.family.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFamilyMongdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFamilyMongdoApplication.class, args);
+	}
+}
